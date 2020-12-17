fb6aea26af61c39a593892f225914ca8418f125e
Francesca <fracassani@gmail.com>
2020-12-17 16:49:55 +0100

    Gestione SCSS

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ include_once __DIR__ . '/scripts/database.php';
     <title>AXIOS PHP DISCHI</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link rel="stylesheet" href="./dist/css/main.css">
 </head>
 <body>
@@ -36,17 +37,20 @@ include_once __DIR__ . '/scripts/database.php';
         <div class="list-cd">
             <ul class="collection">
                 <!-- LOGICA PHP -->
-                <li class="album">
-                    <?php foreach ($database as $data ) { ?>
+                <?php foreach ($database as $data ) { ?>
+                    <li class="album">
                         <div class="album-cover">
                             <img src="<?= $data['poster']; ?>"/> 
+                        </div>
+                        <div class="layover">
+                            <i class="far fa-play-circle"></i>
                         </div>
                         <h3 class="title"> <?php echo $data['title']; ?> </h3>
                         <h4 class="autore"> <?php echo $data['author']; ?> </h4>
                         <h3 class="year"> <?php echo $data['year']; ?> </h3>
                         <h4 class="genre"> <?php echo $data['genre']; ?> </h4>
-                    <?php }?>
-                </li>
+                    </li>
+                <?php }?>
             </ul>
         </div>
 
